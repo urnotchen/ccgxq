b0cf1f921fef42c82cac99bfe7eb0905602071d8
chenxi-lg <3463292245@qq.com>
2017-06-07 09:50:30 +0800

    登录问题

--- backend/components/User.php
+++ backend/components/User.php
@@ -9,9 +9,11 @@ class User extends \yii\web\User
 
     public function loginRequired($checkAjax = true, $checkAcceptHeader = true)
     {
+        var_dump( $this->loginUrl);
+        var_dump( Yii::$app->getRequest()->getAbsoluteUrl());
         $this->loginUrl .= '?return_url=' . Yii::$app->getRequest()->getAbsoluteUrl();
 
-        return parent::loginRequired($checkAjax, $checkAcceptHeader);
+//        return parent::loginRequired($checkAjax, $checkAcceptHeader);
     }
 
     /**
